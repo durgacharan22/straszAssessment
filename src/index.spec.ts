d2d0b076045e126f85501ebf739b818723e8a816
durgacharan22 <durgacharanpotukuru@gmail.com>
2020-06-12 13:05:29 -0400

    initial setup

---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import { sum } from './index';
+
+describe('Addition of Two Number functionality test', () => {
+    test("Add 1 + 2 should be 3", () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+console.log("running properly");
+
+export const sum = (a : number, b : number) => {return a+b}
